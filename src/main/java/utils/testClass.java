24801063d6b2657e151d1affe9fe393559e298ae
Debarnab Banerjee <debarnab.banerjee@gmail.com>
2017-07-09 10:30:35 +0200

    second version got the reflection logic now

--- src/main/java/utils/testClass.java
+++ src/main/java/utils/testClass.java
@@ -1,12 +1,30 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by Debar on 7/7/2017.
  */
 public class testClass {
 
-    public static void main(String[] args) {
-        utiliyFactory.getutiliyInstance();
-        utiliyFactory.instantiateClass("HomePage","dummy");
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+
+        String methodName ="returnString";
+        Class<?> cls = Class.forName("utils.myString");
+        Constructor<?> cons = cls.getConstructor(String.class);
+        //The call to getConstructor specifies that you want the constructor that takes a single String parameter. Now to create an instance:
+        Object o = cons.newInstance("myString");
+        Method m = cls.getMethod(methodName);
+        Object value = m.invoke(o);
+        System.out.println(value.toString());
+
+
+
+
+
     }
 }
